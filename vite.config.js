2ea56d9b0ea1459503b1cc364be424e394c22664
K ANIL KUMAR <kalelynooranilkumar16@gmail.com>
2026-09-13 04:23:29 +0530

    updated the path in vite.config.js and udpate the image path in index.html

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/model-portfolio/',
   plugins: [
     tailwindcss(),
   ],
